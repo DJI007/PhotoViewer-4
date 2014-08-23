d48609ebeedcff93725cf4c04f07409cd93205c0
cachirulop <dmagrom@gmail.com>
2014-08-23 09:16:02 +0200

    Testing

--- photoviewer.cpp
+++ photoviewer.cpp
@@ -1,6 +1,12 @@
 #include "photoviewer.h"
 #include "ui_photoviewer.h"
 
+/**
+ * PhotoView application to view pictures in a simple mode
+ *
+ * @brief PhotoViewer::PhotoViewer
+ * @param parent
+ */
 PhotoViewer::PhotoViewer(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::PhotoViewer)
